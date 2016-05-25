e187a401dbccb0f85bc8749837778cca25f0317f
Jim McAndrew <jim@loc8.us>
2016-05-25 15:31:16 -0600

    Use source instead of raw connection

--- src/connect.js
+++ src/connect.js
@@ -5,25 +5,26 @@ var OAuth = require('oauth').OAuth;
 // Oauthify superagent
 require('superagent-oauth')(superagent);
 
-var initialize = function (connection) {
+var initialize = function (source) {
   return new Promise(function (resolve, reject) {
     var oauth = new OAuth(
-      'http://' + connection.address + 'oauth/request_token',
-      'http://' + connection.address + 'oauth/access_token',
-      connection.consumer_key,
-      connection.consumer_secret,
+      'http://' + source.connection.address + 'oauth/request_token',
+      'http://' + source.connection.address + 'oauth/access_token',
+      source.connection.consumer_key,
+      source.connection.consumer_secret,
       '1.0',
       null,
       'HMAC-SHA1');
     // Get User Details /0.6/user/details
-    superagent.get(connection.address + '0.6/user/details.json')
-      .sign(oauth, connection.access_key, connection.access_secret)
+    console.log(source.connection.address + '0.6/user/details.json');
+    superagent.get(source.connection.address + '0.6/user/details.json')
+      .sign(oauth, source.connection.access_key, source.connection.access_secret)
       .end(function (err, res) {
         if (!err && res.status === 200) {
           resolve({
             oauth: oauth,
             user: res.body,
-            connection: connection
+            connection: source.connection
           });
         } else {
           reject(new Error(err));
