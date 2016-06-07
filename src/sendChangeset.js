918e99dc1345667a15e3ca1affe5e93308215ad2
Jim McAndrew <jim@loc8.us>
2016-06-07 14:38:46 -0600

    Add options: Update src/sendChangeset.js

--- src/sendChangeset.js
+++ src/sendChangeset.js
@@ -5,36 +5,32 @@ var openChangeset = require('./changeset/open');
 var postChangeset = require('./changeset/post');
 var tools = require('jm-tools');
 
-module.exports = function (data, type, osmConnection) {
+module.exports = function (data, type, osmConnection, options) {
   var taskList = [{
-      'name': 'open changeset',
-      'description': 'open a changeset on the OSM server',
-      'task': openChangeset,
-      'params': [osmConnection]
-    }, {
-      'name': 'convert to xml',
-      'description': 'Then we can convert that geojson to XML for the places server',
-      'task': geojsonToOsm,
-      'params': ['changeset', '{{open changeset}}', data, {
-        'generator': 'Places Sync',
-        'changeType': type,
-        'prettyPrint': true
-      }]
-    }, {
-      'name': 'post data to openstreetmap',
-      'description': 'Then we can run that on the places server',
-      'task': postChangeset,
-      'params': [osmConnection, '{{open changeset}}', '{{convert to xml}}']
-    }, {
-      'name': 'close changeset',
-      'description': 'Closing the changeset adds it to places',
-      'task': closeChangeset,
-      'params': [osmConnection, '{{open changeset}}']
-    }, {
-      'name': 'create result',
-      'description': 'Gathers the result data and reports the OSM id for a primary key',
-      'task': createResult,
-      'params': ['{{post data to openstreetmap}}', '{{convert to xml}}']
-    }];
-    return tools.iterateTasks(taskList);
+    'name': 'open changeset',
+    'description': 'open a changeset on the OSM server',
+    'task': openChangeset,
+    'params': [osmConnection]
+  }, {
+    'name': 'convert to xml',
+    'description': 'Then we can convert that geojson to XML for the places server',
+    'task': geojsonToOsm,
+    'params': ['changeset', '{{open changeset}}', data, options]
+  }, {
+    'name': 'post data to openstreetmap',
+    'description': 'Then we can run that on the places server',
+    'task': postChangeset,
+    'params': [osmConnection, '{{open changeset}}', '{{convert to xml}}']
+  }, {
+    'name': 'close changeset',
+    'description': 'Closing the changeset adds it to places',
+    'task': closeChangeset,
+    'params': [osmConnection, '{{open changeset}}']
+  }, {
+    'name': 'create result',
+    'description': 'Gathers the result data and reports the OSM id for a primary key',
+    'task': createResult,
+    'params': ['{{post data to openstreetmap}}', '{{convert to xml}}']
+  }];
+  return tools.iterateTasks(taskList);
 };
