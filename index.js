cdc4f62ab39adcb918336575beab8d021f80f696
Jim McAndrew <jim@loc8.us>
2016-06-07 14:45:30 -0600

    add options: Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var connect = require('./src/connect.js');
 var submit = require('./src/submit.js');
 
 
-module.exports = function(connectionInformation) {
+module.exports = function(connectionInformation, options) {
   return connect(connectionInformation).then(function(connection){
-    return new submit(connection);
+    return new submit(connection, options);
   });
 }
