d96f9b0e9d66887fb5fe498c4c4057953e7277c4
Jim McAndrew <jim@loc8.us>
2016-06-07 14:38:46 -0600

    Add options: Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var connect = require('./src/connect.js');
 var submit = require('./src/submit.js');
-var geojsonToOsm = require('geojsonToOsm');
 
 
 module.exports = function(connectionInformation) {
