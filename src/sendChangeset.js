55d13621000fd1475489170b867650aab71cd9d7
Jim McAndrew <jim@loc8.us>
2016-06-25 07:51:01 -0600

    Fix the nd and way assignments

--- src/sendChangeset.js
+++ src/sendChangeset.js
@@ -12,61 +12,59 @@ var sendChangeset = function (data, type, osmConnection, options) {
   options.returnJson = true;
   options.changesetElements = options.changesetElements || 1;
 
-  return new Promise(function (resolve, reject) {
-
-    // Go through the changes and make a countable array out of it
-    var changeArray = [];
-    var largeChangeset = geojsonToOsm('changeset', '-1', data, options);
-    ['node', 'way', 'relation'].forEach(function (elementType) {
-      if (largeChangeset[elementType] && Array.isArray(largeChangeset[elementType])) {
-        largeChangeset[elementType].forEach(function (change) {
-          changeArray.push({
-            'type': elementType,
-            'data': change
-          });
+  // Go through the changes and make a countable array out of it
+  var changeArray = [];
+  var largeChangeset = geojsonToOsm('changeset', '-1', data, options);
+  ['node', 'way', 'relation'].forEach(function (elementType) {
+    if (largeChangeset[elementType] && Array.isArray(largeChangeset[elementType])) {
+      largeChangeset[elementType].forEach(function (change) {
+        changeArray.push({
+          'type': elementType,
+          'data': change
         });
-      }
-    });
-
-    // Join that countable array up into "bite sized" changesets
-    var jsonChangesets = [];
-    for (var i = 0; i < changeArray.length; i += options.changesetElements) {
-      var tempObj = {};
-      for (var j = 0; j < options.changesetElements && (j < (changeArray.length - i)); j++) {
-        var element = changeArray[i + j];
-        tempObj[element.type] = tempObj[element.type] || [];
-        tempObj[element.type].push(element.data);
-      }
-      jsonChangesets.push(JSON.parse(JSON.stringify(tempObj)));
+      });
     }
-
-    // Create a task list of changesets to run
-    var tasks = [];
-    jsonChangesets.forEach(function (jsonChangeset, i) {
-      if (i > 0) {
-        tasks.push({
-          'name': 'updatedChangesetValues',
-          'description': 'If values within the changeset are assigned new ids, this will update them',
-          'task': updateChangesetValues,
-          'params': [jsonChangeset, '{{compiledChangesetResults' + (i - 1) + '}}']
-        });
-      }
-      tasks.push({
-        'name': 'changeset' + i,
-        'description': 'commits changeset to database',
-        'task': commitChangeset,
-        'params': [jsonChangeset, osmConnection, options]
-      });
-      tasks.push({
-        'name': 'compiledChangesetResults' + i,
-        'description': 'combines the results from the most recent changeset with the previous ones that ran',
-        'task': createMasterResult,
-        'params': ['{{changeset' + i + '.createResult}}', (i > 0 ? ('{{compiledChangesetResults' + (i - 1) + '}}') : null)]
-      });
-    });
-
-    tools.iterateTasks(tasks);
   });
+
+  // Join that countable array up into "bite sized" changesets
+  var jsonChangesets = [];
+  for (var i = 0; i < changeArray.length; i += options.changesetElements) {
+    var tempObj = {};
+    for (var j = 0; j < options.changesetElements && (j < (changeArray.length - i)); j++) {
+      var element = changeArray[i + j];
+      tempObj[element.type] = tempObj[element.type] || [];
+      tempObj[element.type].push(element.data);
+    }
+    jsonChangesets.push(JSON.parse(JSON.stringify(tempObj)));
+  }
+
+  // Create a task list of changesets to run
+  var tasks = [];
+  jsonChangesets.forEach(function (jsonChangeset, i) {
+    if (i > 0) {
+      tasks.push({
+        'name': 'updatedChangesetValues',
+        'description': 'If values within the changeset are assigned new ids, this will update them',
+        'task': updateChangesetValues,
+        'params': [jsonChangeset, '{{compiledChangesetResults' + (i - 1) + '}}']
+      });
+    }
+    tasks.push({
+      'name': 'changeset' + i,
+      'description': 'commits changeset to database',
+      'task': commitChangeset,
+      'params': [jsonChangeset, osmConnection, options]
+    });
+    tasks.push({
+      'name': 'compiledChangesetResults' + i,
+      'description': 'combines the results from the most recent changeset with the previous ones that ran',
+      'task': createMasterResult,
+      'params': ['{{changeset' + i + '.createResult}}', (i > 0 ? ('{{compiledChangesetResults' + (i - 1) + '}}') : null)]
+    });
+  });
+
+  // Run that task list
+  return tools.iterateTasks(tasks);
 };
 
 var updateChangesetId = function (jsonChangeset, changesetId) {
@@ -82,13 +80,11 @@ var updateChangesetId = function (jsonChangeset, changesetId) {
 };
 
 var updateChangesetValues = function (jsonChangeset, previousMasterResult) {
-  console.log('jsonChangeset', JSON.stringify(jsonChangeset, null, 2));
-  console.log('previousMasterResult', JSON.stringify(previousMasterResult, null, 2));
   var newJsonChangeset = {};
 
   var matchToRecord = function (elementType, element) {
     return previousMasterResult.filter(function (result) {
-      return result.osmType === elementType && result.oldId === element.id && (element.version ? result.osmVersion === element.version : true);
+      return result.osmType === elementType && result.oldId === element.id;
     })[0];
   };
 
@@ -102,10 +98,11 @@ var updateChangesetValues = function (jsonChangeset, previousMasterResult) {
       if (element.nd) {
         element.nd = element.nd.map(function (nd) {
           var ndElement = {
-            'id': nd.ref
+            'id': nd.ref.toString()
           };
           matchedRecord = matchToRecord('node', ndElement);
           nd.ref = matchedRecord ? matchedRecord.osmId : nd.ref;
+          return nd;
         });
       }
 
@@ -113,10 +110,11 @@ var updateChangesetValues = function (jsonChangeset, previousMasterResult) {
       if (element.member) {
         element.member = element.member.map(function (member) {
           var memberElement = {
-            'id': member.ref
+            'id': member.ref.toString()
           };
-          matchedRecord = matchedRecord(member.type, memberElement);
+          matchedRecord = matchToRecord(member.type, memberElement);
           member.ref = matchedRecord ? matchedRecord.osmId : member.ref;
+          return member;
         });
       }
 
@@ -124,7 +122,6 @@ var updateChangesetValues = function (jsonChangeset, previousMasterResult) {
     });
   });
 
-  console.log('newJsonChangeset', JSON.stringify(newJsonChangeset, null, 1));
   return newJsonChangeset;
 };
 
@@ -167,7 +164,7 @@ var commitChangeset = function (changesetJson, osmConnection, options) {
     'task': closeChangeset,
     'params': [osmConnection, '{{openChangeset}}']
   }, {
-    'name': 'create result',
+    'name': 'createResult',
     'description': 'Gathers the result data and reports the OSM id for a primary key',
     'task': createResult,
     'params': ['{{post data to openstreetmap}}', '{{xmlChangeset}}', options]
