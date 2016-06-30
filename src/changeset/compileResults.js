d4b0d9ef84a3c36c9fe8169ddbc780abe564bf87
Jim McAndrew <jim@loc8.us>
2016-06-30 12:07:55 -0600

    Remove console log

--- src/changeset/compileResults.js
+++ src/changeset/compileResults.js
@@ -1,7 +1,6 @@
 var arrayify = require('jm-tools').arrayify;
 // Compiles all the results into one object
 module.exports = function (results) {
-  console.log(results);
   var returnValue = [];
   arrayify(results).forEach(function (result) {
     arrayify(result[result.length - 1]).forEach(function (record) {
