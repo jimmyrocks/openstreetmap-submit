9b3a7c6f17066212643a3f4aac569fa3f7c1c027
Jim McAndrew <jim@loc8.us>
2016-07-15 12:41:51 -0600

    Move sendChangeset to its own file

---
+++ src/changeset/send/sendChangeset.js
@@ -0,0 +1,45 @@
+var chunkArray = require('./chunkArray');
+var copy = require('./copy');
+var generateTasks = require('./generateTasks');
+var geojsonToOsm = require('geojsonToOsm');
+var osmJsonToArray = require('./osmJsonToArray');
+var tools = require('jm-tools');
+
+module.exports = function (data, type, osmConnection, options) {
+  var newOptions = copy(options) || {};
+  newOptions.changeType = type;
+  newOptions.changesetElements = newOptions.changesetElements || 1000;
+  newOptions.returnJson = true;
+
+  var tasks = [{
+    'name': 'getOsmJson',
+    'description': 'Gets the geojson in OSM format, but converted to JSON',
+    'task': geojsonToOsm,
+    'params': ['changeset', '-1', data, newOptions]
+  }, {
+    'name': 'convertedArray',
+    'description': 'Convert the OSM JSON to an array for easy counting / splitting',
+    'task': osmJsonToArray,
+    'params': ['{{getOsmJson}}']
+  }, {
+    'name': 'chunkedArray',
+    'description': 'Joins the countable array up into "bite sized" changesets',
+    'task': chunkArray,
+    'params': ['{{convertedArray}}', newOptions.changesetElements]
+  }, {
+    'name': 'taskList',
+    'description': 'Create tasks for each part of that chunked array',
+    'task': generateTasks,
+    'params': ['{{chunkedArray}}', osmConnection, newOptions]
+  }, {
+    'name': 'completedTaskList',
+    'description': 'Runs the task list',
+    'task': function (tasks, name, osmJson) {
+      return tools.iterateTasksLight(tasks, name, true).then(function (results) {
+        return results[tasks.length - 1];
+      });
+    },
+    'params': ['{{taskList}}', 'splitChangesetTaskList', '{{getOsmJson}}']
+  }];
+  return tools.iterateTasksLight(tasks, 'sendChangeset', true);
+};
